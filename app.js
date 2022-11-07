1e844c5c3b1897840218456695a7b4592ab8f41f
Vikram Sungadi <55626981+Vikramsungadi@users.noreply.github.com>
2022-11-07 14:14:28 +0530

    Refactored code.

--- app.js
+++ app.js
@@ -12,113 +12,170 @@ push_input.addEventListener("keydown", addToStack);
 pop.addEventListener("click", removeFromStack);
 pop_input.addEventListener("keydown", removeFromStack);
 
+// document.addEventListener("keydown", (e) => {
+//   stack_container.innerText+=e.code
+// })
+// CHECKING ENTER KEY
 function checkEnter(e) {
-  if (e.code == "Enter" || e.code == "NumpadEnter") {
+  if (e.code == "Enter" || e.code == "NumpadEnter" || e.keyCode === 13) {
+
     return true;
   } else return false;
 }
-
+// PUSH OPERATION
 let index = 0;
 function addToStack(e) {
   if (e.target.id == "push" || checkEnter(e)) {
-    let val = push_input.value;
-    console.log(val === "");
-    if (val !== "") {
-      let div = document.createElement("div");
-      div.className = "values";
-      div.innerText = val;
-      div.setAttribute("value", index);
-      index += 1;
-      stack_container.appendChild(div);
-      push_input.value = "";
+    let entered_value = push_input.value;
+    let multiple_values
+    if (entered_value.includes(",")) {
+       multiple_values = entered_value.split(",")
+    } else {
+      
+       multiple_values = entered_value.split(" ")
     }
+    
+    async function delayInserting() {
+  
+    for (let val of multiple_values) {
+      await new Promise(resolve => {
+
+          
+        if (val !== "") {
+          let div = document.createElement("div");
+          div.className = "values";
+          div.innerText = val;
+          div.setAttribute("value", index);
+          index += 1;
+          stack_container.appendChild(div);
+          push_input.value = "";
+        }
+        resolve()
+      })
+        await new Promise (resolve=> {
+          setTimeout(() => {
+            resolve()
+          }, 500);
+        }
+      )
+    }
+
+
+    }
+    delayInserting()
+
   }
 }
 
+// POP OPERATION
 const temp = document.querySelector(".temp-container");
 const container = document.querySelector(".container");
 
 function removeFromStack(e) {
   if (e.target.id == "pop" || checkEnter(e)) {
-    let arr = document.querySelectorAll(".values");
+    let arr = Array.from(document.querySelectorAll(".values"));
     let pop_value = pop_input.value;
-    let arr2 = Array.from(arr)
-      .slice(parseInt(pop_value) + 1)
-      .reverse();
+    let arr2 = arr.slice(parseInt(pop_value) + 1).reverse();
+    let valueToBeDeleted = arr[parseInt(pop_value)]
+valueToBeDeleted.style.setProperty("background-color","#eed202")
     let delay = 0;
 
     if (pop_value !== "") {
       // ADDING TEMP CONTAINER
       temp.classList.remove("none");
       //   REMOVING FROM STACK
-      arr2.forEach((elem) => {
-        delay += 500;
-        setTimeout(() => {
-          elem.style.animation = "lift 0.5s ease";
-          setTimeout(() => {
-            //   CLONING NODE BEFORE DELETING
-            a = elem.cloneNode(true);
+      async function movingToTemp() {
+        // REMOVING FROM STACK AND ADDING TO TEMP
 
-            elem.remove();
-          }, 480);
-
-          // ADDING TO TEMP FROM STACK
-          setTimeout(() => {
-            a.style.animation = "drop 0.5s ease";
-            temp.appendChild(a);
-          }, 500);
-        }, delay);
-      });
-      console.log(delay);
-
-      setTimeout(() => {
-        let present_stack = document.querySelector(".stack-container");
-
-        last = present_stack.lastChild;
-        last.style.animation = "delete 2s ease";
-        // console.log(last);
-
-        setTimeout(() => {
-          last.remove();
-
-          const present_temp = document.querySelector(".temp-container");
-          temp_array = Array.from(
-            present_temp.querySelectorAll(".values")
-          ).reverse();
-
-          let dela = 0;
-          temp_array.forEach((temp_value) => {
-            dela += 500;
-
-            setTimeout(() => {
-              temp_value.style.animation = "lift 0.5s ease";
-              setTimeout(() => {
-                x = temp_value.cloneNode(true);
-
-                temp_value.remove();
-              }, 480);
-
-              setTimeout(() => {
-                x.style.animation = "drop 0.5s ease";
-
-                stack_container.appendChild(x);
-              }, 500);
-            }, dela);
+        for (let elem of arr2) {
+          await new Promise((resolve) => {
+            elem.style.animation = "lift 0.5s ease";
+            resolve();
           });
-          console.log(dela);
-        }, 1900);
-      }, arr2.length * 700);
 
-      setTimeout(() => {
+          await new Promise((resolve) => {
+            a = elem.cloneNode(true);
+            resolve();
+          });
+
+          await new Promise((resolve) => {
+            a.style.animation = "drop 0.5s ease";
+            resolve();
+          });
+
+          await temp.appendChild(a);
+
+          await new Promise((resolve) => {
+            setTimeout(() => {
+              elem.remove();
+
+              resolve();
+            }, 480);
+          });
+        }
+
+        let present_stack = document.querySelector(".stack-container");
+        let last = present_stack.lastChild;
+        last.style.animation = "delete 2s ease";
+
+        // DELETING VALUE
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            last.remove();
+
+            resolve();
+          }, 1950);
+        });
+
+        // REMOVING FROM TEMP AND ADDING TO STACK
+        const present_temp = document.querySelector(".temp-container");
+        temp_array = Array.from(
+          present_temp.querySelectorAll(".values")
+        ).reverse();
+
+        for (let temp_value of temp_array) {
+          await new Promise((resolve) => {
+            temp_value.style.animation = "lift 0.5s ease";
+            resolve();
+          });
+
+          await new Promise((resolve) => {
+            x = temp_value.cloneNode(true);
+            resolve();
+          });
+
+          await new Promise((resolve) => {
+            x.style.animation = "drop 0.5s ease";
+            resolve();
+          });
+
+          await stack_container.appendChild(x);
+
+          await new Promise((resolve) => {
+            setTimeout(() => {
+              temp_value.remove();
+
+              resolve();
+            }, 480);
+          });
+        }
+
+        // REMOVING TEMP CONTAINER
         temp.style.animation = "fadeout 0.5s ease";
 
-        setTimeout(() => {
-          temp.classList.add("none");
-        }, 480);
-      }, arr2.length * 2 * 950);
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            temp.classList.add("none");
+            resolve();
+          }, 480);
+        });
 
-      pop_input.value = "";
-    } else if (pop_value === "") {
+        pop_input.value = "";
+      }
+      movingToTemp();
+    }
+    
+    else if (pop_value === "") {
       stack_container.lastChild.style.animation = "lift 1s ease";
 
       setTimeout(() => {
